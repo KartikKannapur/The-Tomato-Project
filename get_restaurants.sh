5896d1253f451a9586271e7ce46ea40e7a8bfba1
Sachin Saligram <saligramsachin@gmail.com>
2016-01-09 12:42:23 +0530

    Data Added - 500

--- get_restaurants.sh
+++ get_restaurants.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for i in {1..100}; do
+for i in {1..500}; do
         #curl -X GET --header "Accept: application/json" --header "user_key: a6277adefda4f8fe1521536b33426296" "https://developers.zomato.com/api/v2.1/restaurant?res_id=%d" $i
-        curl -X GET --header "Accept: application/json" --header "user_key: a6277adefda4f8fe1521536b33426296" "https://developers.zomato.com/api/v2.1/restaurant?res_id=$i"
+        curl -X GET --header "Accept: application/json" --header "user_key: 7749b19667964b87a3efc739e254ada2" "https://developers.zomato.com/api/v2.1/restaurant?res_id=$i"
         printf "\n"
         #curl -O $url
 done
