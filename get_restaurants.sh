3a9144dbfe11ecd249123b78a88e61ee0ca312ae
Sachin Saligram <saligramsachin@gmail.com>
2016-01-13 21:51:08 +0530

    (102745-500) data pulls stored in restaurants_complete1

--- get_restaurants.sh
+++ get_restaurants.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in {1..500}; do
+for i in {501..102715}; do
         #curl -X GET --header "Accept: application/json" --header "user_key: a6277adefda4f8fe1521536b33426296" "https://developers.zomato.com/api/v2.1/restaurant?res_id=%d" $i
         curl -X GET --header "Accept: application/json" --header "user_key: 7749b19667964b87a3efc739e254ada2" "https://developers.zomato.com/api/v2.1/restaurant?res_id=$i"
         printf "\n"
