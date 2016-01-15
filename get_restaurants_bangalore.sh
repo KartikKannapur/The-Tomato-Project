ca959f91c2008322ca5efa4217101ae0d03f2a2f
Kartik Kannapur <kartikkannapur@gmail.com>
2016-01-15 12:03:21 +0530

    Bangalore Restaurants - Bash

--- get_restaurants_bangalore.sh
+++ get_restaurants_bangalore.sh
@@ -1,1 +1,7 @@
-curl -X GET --header "Accept: application/json" --header "user_key: a6277adefda4f8fe1521536b33426296" "https://developers.zomato.com/api/v2.1/search?entity_id=4&entity_type=city"
+#!/bin/bash
+for i in $(seq 0 20 80)
+do
+        #curl -X GET --header "Accept: application/json" --header "user_key: a6277adefda4f8fe1521536b33426296" "https://developers.zomato.com/api/v2.1/restaurant?res_id=%d" $i
+        curl -X GET --header "Accept: application/json" --header "user_key: a6277adefda4f8fe1521536b33426296" "https://developers.zomato.com/api/v2.1/search?entity_id=4&entity_type=city&start=" $i
+        #curl -O $url
+done
